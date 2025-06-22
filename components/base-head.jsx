129e84c44d2802ea010925bf307182493a874d94
rcraftzy <roberto.tg99@gmail.com>
2025-06-22 11:04:00 -0500

    fix(paht): resolve path directory favicons

--- components/base-head.jsx
+++ components/base-head.jsx
@@ -22,14 +22,14 @@ const BaseHead = ({ title, description }) => {
       <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
       <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
       <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
       <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
       <link rel="manifest" href="/favicon/manifest.json" />
       <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
       <link rel="stylesheet" href="/styles/styles.css" />
       <script id="theme" src="/scripts/theme.js" />
     </Head>
