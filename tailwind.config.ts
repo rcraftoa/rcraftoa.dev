be2af2ce0b0ed883c2001ab14ce3d790e8ae22f7
rcraftzy <roberto.tg99@gmail.com>
2025-10-18 12:15:13 -0500

    feat: remove typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,8 @@
 import { type Config } from "tailwindcss";
-import typography from "typography";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
   darkMode: "class",
-  plugins: [typography],
 } satisfies Config;
